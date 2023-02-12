193b115b1d580c7acd3703fd269929b25b2fda6f
Franc <angelvargasherasme@gmail.com>
2023-02-12 18:51:29 -0400

    Cambios
    se agrego contenido CSS, Javasscript y se hizo mas atractiva la pagina

---
+++ script/script.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+  document.querySelector(".posicionar").classList.add("ocultar");
+}, 2000);
+
+window.onload = function() {
+    document.querySelector("#Titulo_principal").classList.add("fadeInDown");
+};  
