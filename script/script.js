77f20d20dafe354a4886e5c774f5ac7be63aabdc
Franc <angelvargasherasme@gmail.com>
2023-02-13 00:11:22 -0400

    Se agrego la Bandera Nacional y agrego animaciones y cambios en el css y en la carpeta img

--- script/script.js
+++ script/script.js
@@ -2,6 +2,6 @@ setTimeout(function () {
   document.querySelector(".posicionar").classList.add("ocultar");
 }, 2000);
 
-window.onload = function() {
-    document.querySelector("#Titulo_principal").classList.add("fadeInDown");
-};  
+const titulo = document.querySelector('#Titulo_principal');
+titulo.classList.add('animate__fadeInRight');
+
